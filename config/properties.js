56dcd792bc899ef214d9ed7fddf80d7e34f63921
benAyab <ayabben@protonmail.com>
2022-10-30 00:43:04 +0100

    Modifying Port

--- config/properties.js
+++ config/properties.js
@@ -1,5 +1,5 @@
  module.exports = {
-     PORT: 3000,
+     PORT: process.env.PORT || 3000,
      LOCAL_DB: 'mongodb://localhost:27017/licenseDB',
      REMOTE_DB: 'mongodb+srv://benayab:9Gh6yiqHfk0pOArE@licenseskeycluster.fnnwb62.mongodb.net/?retryWrites=true&w=majority'
  }
